416f9ec7ab4495c6b3d3a58723b88cb389f16392
Will Howard <willh91@msn.com>
2018-10-08 19:55:15 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  // other stuff goes here render etc
+
+}
+
+export default App;
